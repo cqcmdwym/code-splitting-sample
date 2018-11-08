1df2024f4de44ff09a9e48cfab318b437d32a330
cooper.chen <cooper.chen@farfetch.com>
2018-11-08 14:25:06 +0800

    original checkin

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import PageOne from './pageOne';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/pageOne">Page One</Link></li>
+        </ul>
+  
+        <hr/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/pageOne" component={PageOne}/>
+        </Switch>
       </div>
+    </Router>
     );
   }
 }
---
+++ src/pageOne.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PageOne = () => (
+    <div>
+        <h1>PageOne</h1>
+    </div>
+)
+
+export default PageOne
